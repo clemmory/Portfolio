0be9a3af2cd33505e8bbb5d8255ec3d96c4a4a28
Clementine <clementine.mory@gmail.com>
2023-11-13 11:48:48 +0100

    seo

--- components/Home.js
+++ components/Home.js
@@ -24,6 +24,7 @@ function Home() {
       <Head>
         <title>Portfolio Clémentine Mory</title>
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <link rel="icon" href='./public/favicon.ico'/>
       </Head>
       <header className={styles.header}>
         <div className={styles.tabsContainer}>
--- pages/_app.js
+++ pages/_app.js
@@ -7,8 +7,9 @@ function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Portfolio - Clémentine Mory</title>
-        <link rel="icon" href="/favicon.ico"/>
+        <title>Portfolio-Clémentine Mory</title>
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <link rel="icon" href='./public/favicon.ico'/>
       </Head>
       <Component {...pageProps} />
     </>
