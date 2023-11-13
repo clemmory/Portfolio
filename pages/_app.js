692849438afb4b5797aabab70eb6a47d0432e2a6
Clementine <clementine.mory@gmail.com>
2023-11-13 11:20:05 +0100

    meta icon

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,13 @@ import '../styles/globals.css';
 import Head from 'next/head';
 import ReactGA from 'react-ga';
 
-ReactGA.initialize('G-463LQBV470');
-
 
 function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Next.js App</title>
+        <title>Portfolio - Clémentine Mory</title>
+        <link rel="icon" href="/favicon.ico"/>
       </Head>
       <Component {...pageProps} />
     </>
