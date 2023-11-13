0f1eb5c1a038b0971bffb03c65af6cc8d4ae7c83
Clementine <clementine.mory@gmail.com>
2023-11-13 12:15:27 +0100

    meta dats

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,16 @@
 import '../styles/globals.css';
 import Head from 'next/head';
-import ReactGA from 'react-ga';
 
 
 function App({ Component, pageProps }) {
+
+  const icone = ''
   return (
     <>
       <Head>
         <title>Portfolio Clémentine Mory</title>
         <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <link rel="icon" href='/public/icon_portfolio.png'/>
+        <link rel="icon" href='../public/icon_portfolio.png'/>
         <meta name='description' content="Clémentine Mory is a web and mobile developer based in Valencia, Spain."/>
       </Head>
       <Component {...pageProps} />
