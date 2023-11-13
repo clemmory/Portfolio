c08b4ce41c8e46801a98db15e30cd6a7469774fc
Clementine <clementine.mory@gmail.com>
2023-11-13 12:12:36 +0100

    meta datas

--- pages/_app.js
+++ pages/_app.js
@@ -7,9 +7,10 @@ function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Portfolio-Clémentine Mory</title>
+        <title>Portfolio Clémentine Mory</title>
         <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <link rel="icon" href='./public/favicon.ico'/>
+        <link rel="icon" href='/public/icon_portfolio.png'/>
+        <meta name='description' content="Clémentine Mory is a web and mobile developer based in Valencia, Spain."/>
       </Head>
       <Component {...pageProps} />
     </>
