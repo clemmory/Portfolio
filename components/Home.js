8935f20df874622f899a69107de5589123502de5
Clementine <clementine.mory@gmail.com>
2023-11-13 12:29:28 +0100

    head home component

--- components/Home.js
+++ components/Home.js
@@ -24,8 +24,8 @@ function Home() {
       <Head>
         <title>Portfolio Clémentine Mory</title>
         <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <link rel="icon" href='/favicon.ico' />
-        <meta name='description' content="Clémentine Mory is a web and mobile developer based in Valencia, Spain."/>
+        <link rel="icon" href='/icon_portfolio.png' />
+        <meta name="description" content="Clémentine Mory is a web and mobile developer based in Valencia, Spain."/>
       </Head>
       <header className={styles.header}>
         <div className={styles.tabsContainer}>
